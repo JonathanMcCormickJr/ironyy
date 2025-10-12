c93edbb41bad2a402caf0ae1f411e3f142847509
Jonathan McCormick <67705789+JonathanMcCormickJr@users.noreply.github.com>
2025-10-12 16:31:10 -0500

    Accepting defeat

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ Written by Jonathan McCormick Jr. and LGR as part of Jonathan's studies with the
 ## Deviances
 
 ### Database Handling
-While the project originally contained several layers of abstraction for database handling, I have decided to simplify the architecture to have a `.json` file as the database, with a single `DBState` struct to manage interactions with it. This was done to reduce complexity and make the code more maintainable.
 
 ```text
 Initial read of JSON file
@@ -37,7 +36,6 @@ All writes pass through DBState to JSON file
 ```
 
 ### UI Prompts / Actions
-The original design included a `Prompts` struct with dynamic dispatch for various user prompts. However, this added unnecessary complexity. I have simplified the design to use a more straightforward approach without dynamic dispatch, as the set of functions is fixed and does not require such flexibility.
 
 ```text
 
--- src/models.rs
+++ src/models.rs
@@ -1,26 +1,86 @@
-use uuid::Uuid;
-use serde::{Deserialize, Serialize};
+use serde::{Serialize, Deserialize};
+use std::{collections::HashMap, fmt::Display};
 
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub enum Status {
     Open,
     InProgress,
-    Closed,
+    Resolved,
+    Closed
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+impl Display for Status {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Open => {
+                write!(f, "OPEN")
+            }
+            Self::InProgress => {
+                write!(f, "IN PROGRESS")
+            }
+            Self::Resolved => {
+                write!(f, "RESOLVED")
+            }
+            Self::Closed => {
+                write!(f, "CLOSED")
+            }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
 pub struct Epic {
-    pub id: Uuid,
-    pub title: String,
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![]
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct Story {
+    pub name: String,
     pub description: String,
     pub status: Status,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Story {
-    pub id: Uuid,
-    pub epic_id: Uuid,
-    pub title: String,
-    pub description: String,
-    pub status: Status,
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub struct DBState {
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>
 }
